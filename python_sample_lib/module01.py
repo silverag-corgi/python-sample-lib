0093e870eb359c071f4e912a5176ac46fcbe675f
Rem_ <black.white.red.are.the.best@gmail.com>
2022-01-28 10:44:39 +0900

    refactor: ロガー生成関数を整理

--- python_sample_lib/module01.py
+++ python_sample_lib/module01.py
@@ -3,12 +3,13 @@ from json import load
 from logging import Logger, config, getLogger
 
 
-def configure_log() -> None:
+def get_logger(name: str) -> Logger:
+    __configure_log_env()
+    logger: Logger = getLogger(name)
+    return logger
+
+
+def __configure_log_env() -> None:
     log_config_file: TextIOWrapper = open("./config/log_config.json", "r", encoding="utf-8")
     config.dictConfig(load(log_config_file))
     return None
-
-
-def get_logger(name: str) -> Logger:
-    logger: Logger = getLogger(name)
-    return logger
