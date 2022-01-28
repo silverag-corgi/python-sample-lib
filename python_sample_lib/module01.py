5b4d1598b6e2bf15030b1fdf6cddb9e1b12c3186
Rem_ <black.white.red.are.the.best@gmail.com>
2022-01-28 09:18:18 +0900

    feat: ロガー生成関数を実装

--- python_sample_lib/module01.py
+++ python_sample_lib/module01.py
@@ -1,2 +1,14 @@
-def do_func():
-    print('Library')
+from io import TextIOWrapper
+from json import load
+from logging import Logger, config, getLogger
+
+
+def configure_log() -> None:
+    log_config_file: TextIOWrapper = open("./config/log_config.json", "r", encoding="utf-8")
+    config.dictConfig(load(log_config_file))
+    return None
+
+
+def get_logger(name: str) -> Logger:
+    logger: Logger = getLogger(name)
+    return logger
